ac10163e7173819ffdb9892d1f12cc0db16ed66f
EliDev <eliasss.mb@gmail.com>
2024-05-30 19:31:18 -0300

    resposive

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.h-img-menu-hamburguesa').addEventListener('click', () => {
+  document.querySelector('.h-nav').classList.toggle('show')
+})
+
+// ScrollReveal().reveal('.')
+// ScrollReveal().reveal('.', { delay: 500 })
+// ScrollReveal().reveal('.', { delay: 500 })
+// ScrollReveal().reveal('.', { delay: 500 })
